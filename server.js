43ab9484ae0ff5148c34317c40365f6f419d2df9
tushant-katchin <tushant-katchin@ass-MacBook-Pro.local>
2022-10-18 16:39:41 +0530

    updates

--- server.js
+++ server.js
@@ -3,9 +3,11 @@
 console.log('');
 console.log('//Munzi Backend//');
 console.log('');
-
+require('dotenv').config();
 var res = require('dotenv').config();
-
+const appUtils = require('./lib/appUtils')
+const market = require('./lib/Market/marketDao')
+const MarketUtilities = require('./lib/Market/marketUtilities')
 //Import Config
 const config = require('./lib/config');
 // const socket = require("socket.io");
@@ -30,6 +32,20 @@ const config = require('./lib/config');
 
   // attach the routes to the app
   require('./lib/routes')(app);
+
+setInterval(async () => {
+   const marketdata = await appUtils.getCryptoData()
+   let data
+  if(marketdata){
+      //console.log(marketdata)
+      data = await market.saveMarketData(marketdata)
+      
+      
+  }
+  console.log(data)
+  //await MarketUtilities.getCryptoData()
+}, 10000);
+
 const port=2000
   // start server
   const server = app.listen(port, () => {
